d39ac4e97757e289cd4c22cc625f917834dd0287
Oscar Ortega <128606554+oscaror98@users.noreply.github.com>
2026-06-17 13:34:10 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://user:password@host:5432/database")
 
 engine = create_engine(DATABASE_URL)
 
