83e21c16ba4da3057e05b875990085f0a96bf5dc
Oscar Ortega <128606554+oscaror98@users.noreply.github.com>
2026-06-17 13:30:49 +0200

    upload database.py

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Cambia SOLO la contraseña si es distinta
-DATABASE_URL = "postgresql://postgres:0147@localhost:5432/api_tareas"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
